fc514927795675966c930ad9964ac06b7daa29a1
PierreLeBlond <pierre.lespingal@gmail.com>
2021-11-26 12:05:14 +0100

    feat: Change entry point exports
    This is breaking, `Viewer` now refers to real `Viewer` and `PublicViewer` is exposed.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export { default as Viewer } from "./PublicViewer/PublicViewer";
+export { default as PublicViewer } from "./PublicViewer/PublicViewer";
+export { default as Viewer } from "./Viewer/Viewer";
 export { default as Reflector } from "./Viewer/objects/Reflector/Reflector";
+export { default as Material } from "./Viewer/materials/Material/Material";
