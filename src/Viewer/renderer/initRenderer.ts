e2f6a5729d696608e7e88eae76d4b499e91d9ad5
PierreLeBlond <pierre.lespingal@gmail.com>
2022-04-06 11:29:24 +0200

    feat: Set transparent clear color
    this is BREAKING

--- src/Viewer/renderer/initRenderer.ts
+++ src/Viewer/renderer/initRenderer.ts
@@ -1,12 +1,12 @@
 import {ACESFilmicToneMapping, Color, sRGBEncoding, WebGLRenderer} from 'three';
 
 export default function initRenderer(elementId: string): WebGLRenderer {
-  const renderer = new WebGLRenderer({antialias: true});
+  const renderer = new WebGLRenderer({antialias: true, alpha: true});
 
   const element = document.getElementById(elementId);
 
   renderer.setSize(element.clientWidth, element.clientHeight);
-  renderer.setClearColor(new Color(255, 255, 255));
+  renderer.setClearColor(new Color(255, 255, 255), 0);
 
   renderer.outputEncoding = sRGBEncoding;
   renderer.toneMapping = ACESFilmicToneMapping;
