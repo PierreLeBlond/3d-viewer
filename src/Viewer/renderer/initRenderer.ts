1e334e35a6cccbf377c3f3a586ec32e7b8a87fd3
Lespingal <pierre.lespingal@gmail.com>
2023-08-14 18:18:26 +0200

    chore: Update dependencies

--- src/Viewer/renderer/initRenderer.ts
+++ src/Viewer/renderer/initRenderer.ts
@@ -1,18 +1,14 @@
-import { Color, LinearEncoding, NoToneMapping, WebGLRenderer } from 'three';
+import { Color, LinearSRGBColorSpace, NoToneMapping, WebGLRenderer } from 'three';
 
 export default function initRenderer(element: HTMLElement): WebGLRenderer {
   const renderer = new WebGLRenderer(
     { antialias: true, alpha: true, preserveDrawingBuffer: true });
 
-
   renderer.setSize(element.clientWidth, element.clientHeight);
   renderer.setClearColor(new Color(255, 255, 255), 0);
 
-  renderer.outputEncoding = LinearEncoding;
+  renderer.outputColorSpace = LinearSRGBColorSpace;
   renderer.toneMapping = NoToneMapping;
-
-  // turn on the physically correct lighting model
-  renderer.physicallyCorrectLights = true;
 
   renderer.clearColor();
 
